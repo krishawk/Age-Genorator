d001137ea3a4d0c255603902874f7d2d06a93d67
Kristie Hawkins <74780639+krishawk@users.noreply.github.com>
2022-01-24 17:20:06 -0600

    Add files via upload

---
+++ age generator.py
@@ -0,0 +1,7 @@
+name = input ("Enter your name!")
+age = input("Enter your age!")
+checker = int(age)
+if checker <= 20:
+	print("You are not old enough!!!")
+else:
+		print ("Your are old enough " + name)
